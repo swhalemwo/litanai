d917e60e0772fbeb3960b6ace8999daacb67f0c8
swhalemwo <jojo.ae@hotmail.de>
2024-10-15 19:22:34 +0200

    more functionalization, bundling, additional explorations

--- test_pymupdf.py
+++ test_pymupdf.py
@@ -129,8 +129,6 @@ def gen_initial_db (client) :
 # client.command("SHOW DATABASES")
 
 
-# if __name__ == "__main__":
-
 def gd_reltexts (query_reltext):
     "get a df of the relevant texts"
     
@@ -139,8 +137,10 @@ def gd_reltexts (query_reltext):
     dtx = client.query_df(query_reltext)
 
     # add count of tokens: shouldn't be longer than 128k (gpt-4o-mini limit)
-    encoder = tiktoken.get_encoding("o200k_base")
-    dtx.insert(3, 'tokens', dtx.apply(lambda r: len(encoder.encode(r['text'])), axis = 1))
+
+    if 'text' in dtx.columns:
+        encoder = tiktoken.get_encoding("o200k_base")
+        dtx.insert(3, 'tokens', dtx.apply(lambda r: len(encoder.encode(r['text'])), axis = 1))
 
     
     return(dtx)
@@ -173,20 +173,73 @@ def qry_oai (key, prompt, text_to_query):
     
     res_dict = query_res.to_dict()['choices'][0]['message']['content']
 
-    try:
-        res_json = json.loads(res_dict)['results']
+    res_json = json.loads(res_dict)
 
-    except:
-        pdb.set_trace()
+    if list(res_json.keys()) == ['pagenumber', 'quote', 'reason']:
+        res_json2 = res_json
+
+    elif len(res_json.keys()) == 1:
+        res_key = list(res_json.keys())[0]
+
+        # sometimes key is results, sometimes just result
+        if res_key in ['results', 'result']:
+            res_json2 = res_json[res_key]
+
+        else:
+            pdb.set_trace()
         
-    dt_res = pd.DataFrame(res_json)
+    dt_res = pd.DataFrame(res_json2)
     dt_res.insert(0, "key", key)
 
     return(dt_res)
 
+def gs_oai_prompt (topic, desc):
+    "generate prompt: include extraction steps"
+    
+    ins_gen = f"""quote every instance that you find literally word for word, don't rephrase anything. include all the text of each instance that is necessary for it to be understood when standing on its own. Do not use ellipses, include each instance from start to finish, even if within there are elements that may seem less relevant. If there is some context surrounding the instance which is necessary to understand the instance, include it in the quote. Rather include too much text than too little. it is important that the point of each instance is clearly understandable on its own.
+
+Return the results as a list of dicts so it can be parsed as json, with each dict having the keys pagenumber, quote, and reason why this instance is relevant. if this text does not concern {topic}, return an empty list.
+
+The text follows below this line:
+"""
+
+    prompt_full = desc + "\n\n" + ins_gen
+    return(prompt_full)
+
+
+def litanai (query_reltext, prompt_oai, proj_name, head):
+    "integrated litreview: first get queries from database"
+    
+    
+
+    dt_reltexts = gd_reltexts(query_reltext)
+    print(dt_reltexts.shape)
+
+    
+    if head: 
+        l_res = (dt_reltexts[dt_reltexts['tokens'] < 100000].head()
+                 .apply(lambda row: qry_oai(row['key'], prompt_oai, row['text']), axis = 1))
+    else:
+        l_res = (dt_reltexts[dt_reltexts['tokens'] < 100000]
+                 .apply(lambda row: qry_oai(row['key'], prompt_oai, row['text']), axis = 1))
+    
+    # combine results
+    l_res_cbnd = [res for res in l_res if not res.empty]
+
+    dt_res_cbnd = pd.concat(l_res_cbnd)
+
+    dt_res_cbnd.to_csv(f"{PROJ_DIR}res/res_{proj_name}.csv")
+    print('done')
+
+
+# if __name__ == "__main__":
+
 
 # * main
 
+# ** private museum influence
+
+PROJ_DIR = "/home/johannes/Dropbox/proj/litanai/"
 
 
 query_reltext = "".join(["select key, length(text) AS len, ", 
@@ -197,40 +250,65 @@ query_reltext = "".join(["select key, length(text) AS len, ",
                      " order by n_occur DESC"])
 
 
-dt_reltexts = gd_reltexts(query_reltext)
-pl.DataFrame(dt_reltexts)
+prompt = """you will read a long text. the text is in some way about private art museums, a new form of museums started by wealthy collectors. you have to find every instance in this text about how private art museums have (or have not) an effect on the arts, for example that the artists after being exhibted experience a boost to their career, increase their chances of canonization or consecration, are more likely to be raise higher prices at auctions or are more likely to be exhibited by other museums or institutions. Any impact that private museums leave in the field of artistic production."""
 
-
-dt_reltexts[dt_reltexts['tokens'] < 100000]
-
-prompt = """you will read a long text. the text is in some way about private art museums, a new form of museums started by wealthy collectors. you have to find every instance in this text about how private art museums have (or have not) an effect on the arts, for example that the artists after being exhibted experience a boost to their career, increase their chances of canonization or consecration, are more likely to be raise higher prices at auctions or are more likely to be exhibited by other museums or institutions. Any impact that private museums leave in the field of artistic production.
-
-quote every instance that you find literally word for word, don't rephrase anything. include all the text of each instance that is necessary for it to be understood when standing on its own. Do not use ellipses, include each instance from start to finish, even if inbetween there are elements that may seem less relevant. If there is some context surrounding the quote which is necessary to understand it, include it in the quote. Rather include too much text than too little, it is important that the point of each instance is clearly understandable, and that a quote convey its message on its own. 
-
-Return the results as a list of dicts so it can be parsed as json, with each dict having the keys pagenumber, quote, and reason why this instance is relevant. if this text does not concern canonization/artist careers, return an empty list.
-
-The next follows below this line:
-"""
 
 
 # single query
 jj = qry_oai("Brown_2019_private.pdf", prompt, dt_reltexts.iloc[0]['text'])
 
 
-l_res = (dt_reltexts[dt_reltexts['tokens'] < 100000]
-         .apply(lambda row: qry_oai(row['key'], prompt, row['text']), axis = 1))
+
+# ** nonprofit survival
 
 
+query_nposurv = """select key, len, text, n_surv + n_closing + n_closure + n_cox as surv_ttl, n_np1 + n_np2 as n_np from (
+SELECT key, length(text) as len, text, 
+(LENGTH(text) - LENGTH(REPLACE(text, 'surviv', ''))) / LENGTH('surviv') AS n_surv,
+(LENGTH(text) - LENGTH(REPLACE(text, 'closing', ''))) / LENGTH('closing') AS n_closing,
+(LENGTH(text) - LENGTH(REPLACE(text, 'closure', ''))) / LENGTH('closure') AS n_closure,
+(LENGTH(text) - LENGTH(REPLACE(text, 'cox', ''))) / LENGTH('cox') AS n_cox,
+(LENGTH(text) - LENGTH(REPLACE(text, 'non-profit', ''))) / LENGTH('non-profit') AS n_np1,
+(LENGTH(text) - LENGTH(REPLACE(text, 'nonprofit', ''))) / LENGTH('nonprofit') AS n_np2
+FROM littext)
+where surv_ttl > 8 and n_np > 8"""
+
+dt_rt_npo = gd_reltexts(query_nposurv)
+
+dt_rt_npo[dt_rt_npo['tokens'] < 100000]
 
 
-l_res = dt_reltexts.head().apply(lambda row: qry_oai(row['key'], prompt, row['text']), axis = 1)
+prompt_npo = gs_oai_prompt(
+    "factors associated with non-profit closure",
+    """you will read a text long which is some way related to non-profit organizations. It very likely is in some way about non-profit organizations closing (or surviving). Your task is to identify every instance where the text talks about factors associated with closings. These can be properties of the non-profit organization itself, the environent it is situated in, or really anything. """)
 
-l_res_cbnd = [res for res in l_res if not res.empty]
+litanai(query_reltext = query_nposurv, query_oai = prompt_npo, proj_name = "npo_surv", head =False)
 
-dt_res_cbnd = pd.concat(l_res_cbnd)
-
-PROJ_DIR = "/home/johannes/Dropbox/proj/litanai/"
-
-dt_res_cbnd.to_csv(PROJ_DIR + "res.csv")
+# ** role of individuals
 
 
+prompt_vid = gs_oai_prompt(
+    'the role of individuals in museum closures',
+    """you will read a scientific article which in some way if related to the closure of non-profit organizations. Your task is to identify every instance where the texts talks about the role that individual people play in the closing of non-profit organizations. you should look for factors on the level of the individual person, such as exhaustion, demotivation or conflict within the organization. only report instances where individual-level factors are measured directly.""")
+# Do NOT try to infer individual-level factors from organizational ones, such as organizational age or expenditure; 
+print(prompt_vid)
+
+litanai(query_reltext = query_nposurv, prompt_oai = prompt_vid, proj_name = "surv_vid", head = False)
+
+xx = qry_oai("Fernandez", prompt_vid, dt_rt_npo.iloc[19]['text'])
+xx.to_csv(f"{PROJ_DIR}/res/res_vid_test.csv")
+
+
+query_coords = """select key, n_coords, len, text,  n_surv + n_closing + n_closure + n_cox as surv_ttl, n_np1 + n_np2 as n_np from (
+SELECT key, length(text) as len, text, 
+(LENGTH(text) - LENGTH(REPLACE(text, 'surviv', ''))) / LENGTH('surviv') AS n_surv,
+(LENGTH(text) - LENGTH(REPLACE(text, 'closing', ''))) / LENGTH('closing') AS n_closing,
+(LENGTH(text) - LENGTH(REPLACE(text, 'closure', ''))) / LENGTH('closure') AS n_closure,
+(LENGTH(text) - LENGTH(REPLACE(text, 'cox', ''))) / LENGTH('cox') AS n_cox,
+(LENGTH(text) - LENGTH(REPLACE(text, 'non-profit', ''))) / LENGTH('non-profit') AS n_np1,
+(LENGTH(text) - LENGTH(REPLACE(text, 'nonprofit', ''))) / LENGTH('nonprofit') AS n_np2,
+(LENGTH(text) - LENGTH(REPLACE(text, 'coordinates', ''))) / LENGTH('coordinates') AS n_coords
+FROM littext)
+where surv_ttl > 8 and n_np > 8"""
+
+dt_rt_coords = gd_reltexts(query_coords)
